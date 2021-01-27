625214bfec3a1c59107cb3fc0a6ab5ce81935d80
Galster-dev <segom0574@gmail.com>
2021-01-27 12:31:13 +0300

    moved generic patches to their place

--- src/patches/generic.h
+++ src/patches/generic.h
@@ -1,7 +1,13 @@
 #ifndef GENERIC_H
 #define GENERIC_H
 
-#include "../gamedata/func.h"
+// #include "../gamedata/func.h"
+#include "meeting_hud.h"
+
+void DummyBehaviour_Start_h(DummyBehaviour_o *pThis);
+void VersionShower_Start_h(VersionShower_o *pThis);
+SBYTE PlayerVoteArea_GetVotedFor_h(uint32_t index);
+void KeyMinigame_Start_h(KeyMinigame_o* pThis);
 
 namespace GameDataPatches
 {
